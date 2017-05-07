273e7c73480d18391938ae904cf757a131a0a05b
PontiusPilatus <mailgorinich@gmail.com>
2017-05-07 22:02:57 +0500

    Прочти меня

---
+++ item.js
@@ -0,0 +1,11 @@
+exports.add = function(req,res,itemFields) {
+    //TODO add add method
+};
+
+exports.remove = function(req, res, itemUUID) {
+  //TODO add remove method
+};
+
+exports.change = function(req, res, itemFields) {
+    //TODO add change method
+};
