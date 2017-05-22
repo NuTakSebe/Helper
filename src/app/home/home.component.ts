bc999835f4319138a6925661274c426e9cf9c898
Ilshat Ishmukhametov <ilshatishmukhametov@gmail.com>
2017-05-22 23:07:53 +0500

    fix error in console.log

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
   constructor(private route: ActivatedRoute, private getQueryParamService : GetQueryParamService) {  }
 
   ngOnInit() {
-    console.log = this.getQueryParamService.getParamByName(this.route, "token");
+    console.log(this.getQueryParamService.getParamByName(this.route, "token"));
   }
 
 }
