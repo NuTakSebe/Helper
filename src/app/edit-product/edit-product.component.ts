db8ec67d758084b7f60370d08d173d7478102f45
ishmukhamet <ilshatishmukhametov@gmail.com>
2017-05-29 23:13:07 +0500

    prepare to requests

--- src/app/edit-product/edit-product.component.ts
+++ src/app/edit-product/edit-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-edit-product',
@@ -6,10 +7,116 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-product.component.css']
 })
 export class EditProductComponent implements OnInit {
+  mode = "edit";
 
-  constructor() { }
+  stores = [];
+
+  storeProducts = [];
+
+  tableProducts = [];
+
+  storeUUID = "333";
+
+  oldProduct = {
+    "uuid": 4234234,
+    "code": "",
+    "barCodes": [123123123, 123123, 5555],
+    "alcoCodes": [],
+    "name": "ttetst",
+    "price": 5555,
+    "quantity": 234,
+    "costPrice": 0,
+    "measureName": "шт",
+    "tax": "VAT_0",
+    "allowToSell": true,
+    "description": "",
+    "articleNumber": "",
+    "parentUuid": "null",
+    "group": false,
+    "type": "NORMAL",
+    "alcoholByVolume": 0,
+    "alcoholProductKindCode": 0,
+    "tareVolume": 0
+  }
+
+  deleteProduct(productUUID) {
+    console.log(productUUID);
+
+    // TODO:
+    // запрос на удаление
+    // удалить товар из массивов тут
+  }
+
+  openForm(content, product) {
+   this.modalService.open(content, {size: "lg"});
+   this.oldProduct = Object.assign({}, product);
+ }
+
+ openAlert(content, product) {
+   console.log(product.uuid);
+   this.modalService.open(content, {size: "lg"});
+}
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+    // TODO:
+    // получить список магазинов
+    this.stores = [{
+      "uuid": "string11",
+      "name": "string1",
+      "address": "string"
+    },
+    {
+      "uuid": "string22",
+      "name": "string2",
+      "address": "string"
+    }];
+
+    this.storeProducts = [{
+      "uuid": "test",
+      "code": "434",
+      "barCodes": [33123123, 11111],
+      "alcoCodes": [],
+      "name": "44444",
+      "price": 434,
+      "quantity": 444,
+      "costPrice": 0,
+      "measureName": "шт",
+      "tax": "VAT_0",
+      "allowToSell": true,
+      "description": "123123",
+      "articleNumber": "",
+      "parentUuid": "null",
+      "group": false,
+      "type": "NORMAL",
+      "alcoholByVolume": 0,
+      "alcoholProductKindCode": 0,
+      "tareVolume": 0
+    }, {
+      "uuid": "te11st",
+      "code": "434",
+      "barCodes": [],
+      "alcoCodes": [],
+      "name": "tests14",
+      "price": 434,
+      "quantity": 444,
+      "costPrice": 0,
+      "measureName": "шт",
+      "tax": "VAT_0",
+      "allowToSell": true,
+      "description": "123123",
+      "articleNumber": "",
+      "parentUuid": "null",
+      "group": false,
+      "type": "NORMAL",
+      "alcoholByVolume": 0,
+      "alcoholProductKindCode": 0,
+      "tareVolume": 0
+    }];
+
+    this.tableProducts = this.storeProducts;
+
   }
 
 }
--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { GenerateUUID4Service } from '../generate-uuid4.service';
 
@@ -9,8 +9,9 @@ import { GenerateUUID4Service } from '../generate-uuid4.service';
   providers: [GenerateUUID4Service]
 })
 export class ProductFormComponent implements OnInit {
-
-  constructor(private generateUUID4: GenerateUUID4Service) { }
+  @Input() mode;
+  @Input() oldProduct;
+  @Input() storeUUID;
 
   measures = [
     {
@@ -60,7 +61,7 @@ export class ProductFormComponent implements OnInit {
       value: "дроб",
       displayName: "Дробное"
     }
-  ]
+  ];
 
   taxes = [
     {
@@ -86,7 +87,7 @@ export class ProductFormComponent implements OnInit {
       value: "VAT_18_118",
       displayName: "НДС с расчётной ставкой 18%"
     },
-  ]
+  ];
 
   types = [
     {
@@ -101,16 +102,13 @@ export class ProductFormComponent implements OnInit {
       value: "ALCOHOL_NOT_MARKED",
       displayName: "Немаркированный алкоголь"
     }
-  ]
+  ];
 
-  barCodes = [];
-  alcoCodes = [];
-
-  product = {
+  emptyProduct = {
     "uuid": this.generateUUID4.generate(),
     "code": "",
-    "barCodes": this.barCodes,
-    "alcoCodes": this.alcoCodes,
+    "barCodes": [],
+    "alcoCodes": [],
     "name": "",
     "price": 0,
     "quantity": 0,
@@ -120,7 +118,7 @@ export class ProductFormComponent implements OnInit {
     "allowToSell": true,
     "description": "",
     "articleNumber": "",
-    "parentUuid": "",
+    "parentUuid": null,
     "group": false,
     "type": "NORMAL",
     "alcoholByVolume": 0,
@@ -128,8 +126,11 @@ export class ProductFormComponent implements OnInit {
     "tareVolume": 0
   };
 
+  product = Object.assign({}, this.emptyProduct);
+
   isAlco = false;
 
+  // обновляет поля product при снятии/установлении галочки алкотовара
   updateAlco = function(isAlco) {
     if (!isAlco) {
       this.product.type = "NORMAL";
@@ -145,12 +146,102 @@ export class ProductFormComponent implements OnInit {
     }
   }
 
-  // TODO: ЗАПРОС
-  shops = [];
+  updateGroup = function() {
+    if (this.product.group) {
+      this.product.barCodes.length = 0;
+      this.product.alcoCodes.length = 0;
+      this.product.quantity = 0;
+      this.product.price = 0;
+      this.product.costPrice = 0;
+      this.product.description = "";
+      this.product.measureName = "";
+      this.product.tax = "NO_VAT";
+      this.product.allowToSell = true;
+      this.product.articleNumber = "";
+      this.product.alcoholByVolume = 0;
+      this.product.alcoholProductKindCode = 0;
+      this.product.tareVolume = 0;
+    } else {
+      this.product.measureName = "шт";
+      this.product.tax = "VAT_0";
+    }
+  }
+
+  stores = [];
+
+  storeGroups = [];
+
+  errorNoStores = false;
+  errorNoGroups = false;
+
+  updateGroups = function(storeUUID) {
+    this.storeGroups = [];
+    // TODO: ЗАПРОС
+    this.storeGroups = [{
+      "uuid": "group1",
+      "name": "name"
+    }];
+
+    if (!(this.storeGroups[0])) {
+      this.errorNoGropes = true;
+    }
+  };
+
+  submitFunction;
+
+  constructor(private generateUUID4: GenerateUUID4Service) { }
 
   ngOnInit() {
-    this.product.barCodes.push('test');
-    this.product.alcoCodes.push('blah');
+    if (this.mode === 'add') {
+      // TODO:
+
+      // получить магазины
+      this.stores = [{
+        "uuid": "string11",
+        "name": "string1",
+        "address": "string"
+      },
+      {
+        "uuid": "string22",
+        "name": "string2",
+        "address": "string"
+      }];
+
+      // если магазинов нет - заблокировать форму
+      if (!(this.stores[0])) {
+        this.errorNoStores = true;
+      } else {
+        this.storeUUID = this.stores[0].uuid;
+      }
+
+      // после выбора магазина получить группы - это делает updateGroups;
+
+      // на сабмит вешаем отправку
+      this.submitFunction = function() {
+        alert(this.mode);
+        this.product = Object.assign({}, this.emptyProduct);
+        this.isAlco = false;
+        this.product.barCodes.length = 0;
+        this.product.alcoCodes.length = 0;
+        window.scrollTo(0,0);
+      }
+    } else if (this.mode === 'edit') {
+      // TODO:
+
+      // запихать переданный объект в product
+      this.product = Object.assign({}, this.oldProduct);
+
+      // получить группы по магазину
+      this.storeGroups = [{
+        "uuid": "group1",
+        "name": "name"
+      }];
+
+      // на сабмит навесить удаление и отправку
+      this.submitFunction = function() {
+        alert(this.mode);
+      }
+    }
   }
 
 }
