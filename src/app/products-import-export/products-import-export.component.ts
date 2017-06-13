0cd3acba32de1c55c36b9365d45680d05077e8d1
PontiusPilatus <mailgorinich@gmail.com>
2017-06-13 12:53:29 +0500

    working excel

--- src/app/products-import-export/products-import-export.component.ts
+++ src/app/products-import-export/products-import-export.component.ts
@@ -14,25 +14,24 @@ declare var XLSX: any;
 export class ProductsImportExportComponent implements OnInit {
 
   XLSX: any;
-  form: String= "";
-  jsonExcel = {
-    "name": "Developer",
-    "party": "Development"
-  };
+  form: String = "";
+	static jsonExcel = [];
 
-  constructor(private http: Http) {}
+  constructor() {}
 
+	getJson(){
+		return ProductsImportExportComponent.makeValid();
+	}
   onSubmit(form: string) {
-    const body = null;
     console.log("Нажатие")
-    let xhr = new XMLHttpRequest();
+    const xhr = new XMLHttpRequest();
     xhr.open('POST', '/excel/download?for=' + form, true);
     xhr.responseType = 'blob';
     xhr.send(null);
     xhr.onload = function(e) {
-      if (xhr.status == 200) {
+      if (xhr.status === 200) {
         console.log(xhr.response);
-        var blob;
+        let blob;
         if (form === 'xlsx') {
           blob = new Blob([xhr.response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
         } else {
@@ -48,31 +47,14 @@ export class ProductsImportExportComponent implements OnInit {
         a.click();
         window.URL.revokeObjectURL(url);
       } else {
-        console.log("Error Appeared while downloading " + xhr.status)
+        console.log("Error Appeared while downloading " + xhr.status);
+				alert("Error While Downloading")
       }
     };
   }
 
-  static fixdata(data) {
-    console.log("hi")
-    var o = "",
-      l = 0,
-      w = 10240;
-    for (; l < data.byteLength / w; ++l) o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)));
-    o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)));
-    return o;
-  }
-
-  static upload(file) {
-    console.log("Upload Started");
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "/excel/upload", true);
-    console.log("Sending file Started");
-    xhr.send(file);
-    return xhr.status;
-  }
-
-  handleFile(event: any, jsonExcel: any) {
+  handleFile(event: any) {
+    // TODO проверить валидность расширения
     var rABS = true; // true: readAsBinaryString ; false: readAsArrayBuffer
     var files = event.target.files; // Забираем у event( нажатие ) список выбранных файлов. Возвращает ArrayList of Files
     var i, file; // Объявляем переменные для итерации и сам file
@@ -91,57 +73,208 @@ export class ProductsImportExportComponent implements OnInit {
         });
       } else {
         /* if array buffer, convert to base64 */
-        var arr = ProductsImportExportComponent.fixdata(data);
-        workbook = XLSX.read(btoa(arr), {
+        let arr = ProductsImportExportComponent.fixdata(data);
+        workbook =  XLSX.read(btoa(arr), {
           type: 'base64'
         });
       }
       var worksheet = workbook.Sheets[workbook.SheetNames[0]];
-      jsonExcel = XLSX.utils.sheet_to_json(worksheet);
-      console.log("Tralala")
-      console.log(jsonExcel);
+      ProductsImportExportComponent.jsonExcel = XLSX.utils.sheet_to_json(worksheet);
 
-      // ProductsImportExportComponent.upload(jsonExcel);
-      console.log(status);
+			// console.log(ProductsImportExportComponent.jsonExcel)
     };
     reader.readAsBinaryString(file);
   }
-  /* processing array
-  buffers, only required for readAsArrayBuffer */
+	
+	getType(str) {
+		if (str === "NORMAL") return "обычный";
+		if (str === "ALCOHOL_MARKED") return "маркированный алкоголь";
+		if (str === "ALCOHOL_NOT_MARKED") return "немаркированный алкоголь";
+	}
+	getNDS(str) {
+		if (str === "VAT_0") return "Без НДС";
+		if (str === "VAT_10") return "НДС 10%";
+		if (str === "VAT_18") return "НДС 18%";
+		if (str === "VAT_10_110") return "НДС с расчетной ставкой 10%";
+		if (str === "VAT_18_118") return "НДС с расчетной ставкой 18%";
+	}
 
-send(jsonExcel) {
-  let uuid = 1234567890;
-  let name = jsonExcel["Артикул ↵(до 20 символов)"];
-  let code = jsonExcel["Код товара↵(до 10 символов)"];
-  
-  let json = {
-      "uuid": "string",
-      "code": "string",
-      "barCodes": [
-      "string"
-      ],
-      "alcoCodes": [
-      "string"
-      ],
-      "name": "string",
-      "price": 0,
-      "quantity": 0,
-      "costPrice": 0,
-      "measureName": "",
-      "tax": "NO_VAT",
-      "allowToSell": true,
-      "description": "string",
-      "articleNumber": "string",
-      "parentUuid": "string",
-      "group": true,
-      "type": "NORMAL",
-      "alcoholByVolume": 0,
-      "alcoholProductKindCode": 0,
-      "tareVolume": 0
-    }
+  static makeValid() {
+		
+		let arrJs = [];
+		if (ProductsImportExportComponent.jsonExcel.length !== 0) {
+    for (let num in ProductsImportExportComponent.jsonExcel) {
+      let prd = ProductsImportExportComponent.jsonExcel[num];
+			console.log(prd);
+			if (num != "0") {
+				let errors = "";
+				
+        let uuid = "testUuid"; // TODO make uniq Uuid
+        let code = "";
+        if (prd["Код товара"].length <= 10) { code = prd["Код товара"]; }
+        else {
+          code = "NOT VALLID";
+          errors += "Невалидное поле: Код Товара\n";
+//          throw Error;
+        }
+				let barCode = prd["Штрихкоды"].replace(/ /g,"").split(",");
+				
+        let name = "NOT VALLID";
+        if (prd["Наименование товара"].length <= 100 &&
+          prd["Наименование товара"].length > 0) {
+          name = prd["Наименование товара"];
+        } else {
+          errors += "Невалидное поле: Наименование товара\n";
+//          throw Error;
+        }
+        let price = 0;
+        let buff = parseFloat(prd["Отпускная цена"].replace(/,/g, ".").replace(/ /g,""));
+        if (buff >= 0 && buff <= 9999999.99) { price = buff; }
+        else {
+          errors += "Невалидное поле: Отпускная цена\n";
+//          throw Error;
+        }
+        let quantity = 0;
+        let buffStr = prd["Количество товара в наличии(остаток)"];
+				let arrSplit = buffStr.replace(/ /g,"").replace(/,/g, ".");
+				if (arrSplit.length != 1) {
+					if (buffStr.split(".")[1].length <= 3) { quantity = parseFloat(buffStr); }
+					else {
+						errors += "Невалидное поле: Количество товара в наличи\n";
+//            throw Error;
+					}
+				}else {
+          quantity = parseFloat(buffStr);
+        }
+				
+        let costPrice = 0;
+        buff = parseFloat(prd["Закупочная цена"].replace(/ /g,"").replace(/,/g, "."));
+        if (buff >= 0 && buff <= 9999999.99) { costPrice = buff; }
+        else {
+          errors += "Невалидное поле: Закупочная цен\n";
+//          throw Error;
 
-  console.log("Sended!");
-}
+        }
+				
+        let measureName = "NOT VALLID";
+        buffStr = prd["Единица измерения"].replace(/ /g,"").toLowerCase();
+        if (buffStr == "шт" || buffStr == "кг" || buffStr == "л" || buffStr == "м" || buffStr == "км" || buffStr == "м2" || buffStr == "м3" || buffStr == "компл" || buffStr == "упак" || buffStr == "ед" || buffStr == "дроб") {
+   				measureName = buffStr;
+        } else {
+					errors += "Невалидное поле: Единица измерения\n";
+//          throw Error;
+        }
+        let tax = "NOT VALLID";
+        switch (prd["Ставка НДС"].toLowerCase()) {
+          case "0": tax = "VAT_0"; break;
+          case "8 б": tax = "VAT_18"; break;
+          case "8 с": tax = "VAT_18_118"; break;
+          case "10 б": tax = "VAT_10"; break;
+          case "10 с": tax = "VAT_10_110"; break;
+          default: errors += "Невалидное поле: Ставка НДС\n"; //throw Error;
+        }
+				
+        let typePrd = "NOT VALLID";
+        switch (prd["Тип товара"].toLowerCase()) {
+          case "обычный": typePrd = "NORMAL"; break;
+          case "маркированный алкоголь": typePrd = "ALCOHOL_MARKED"; break;
+          case "немаркированный алкоголь": typePrd = "ALCOHOL_NOT_MARKED"; break;
+          default: errors += "Невалидное поле: Тип товара" + name + "\n"; //throw Error;
+        }
+
+        let allowToSell = false;
+        switch (prd["Доступности товара для продажи"].replace(/ /g,"").toLowerCase()) {
+          case "да": allowToSell = true; break;
+          case "нет": allowToSell = false; break;
+          default: errors += "Невалидное поле: Доступность для продажи\n"; //throw Error;
+        }
+
+				let alcoCodes = [];
+				let alcholByVolume = 0;
+				let alchoholProductKindCode = 0;
+				let tareVolume = 0;
+				if (typePrd !== "NORMAL") {
+					alcoCodes = ["NOT VALLID"];
+					if (prd["Коды алкогольной продукции ЕГАИС"] !== null) {
+						alcoCodes = prd["Коды алкогольной продукции ЕГАИС"].replace(/ /g,"").replace(/,/g,".").split(".");
+					}else {
+						errors += "Невалидное поле: Коды алкогольной продукции ЕГАИС";
+					}
+
+					alcholByVolume = -1;
+					buffStr =  prd["Крепкость алкогольной продукции"].replace(/ /g,"").replace(/,/g,".");
+					arrSplit = buffStr.split(".");
+					if (arrSplit.length != 1) {
+						if (arrSplit[1].length <= 3) { alcholByVolume = parseFloat(buffStr);  }
+						else { errors += "Невалидное поле: Крепость алкогольной продукции\n"; } 
+					}else { alcholByVolume = parseFloat(buffStr); }
+					
+					let buffN = parseInt(prd["Код вида алкогольной продукции ФСРАР"].replace(/ /g,"")); 
+					if ( buffN> 0 && prd["Код вида алкогольной продукции ФСРАР"] < 1000) {alchoholProductKindCode = buffN;}
+					else { errors += "Невалидное поле: Код вида алкогольной продукции ФСРАР\n"}
+					
+					tareVolume = -2;
+					buffStr =  prd["Ёмкость тары алкогольной продукции в литрах"].replace(/ /g,"").replace(/,/g,".");
+					arrSplit = buffStr.split(".");
+					if (arrSplit.length != 1) {
+						if (arrSplit[1].length <= 3) { tareVolume = parseFloat(buffStr);  }
+						else { errors += "Невалидное поле: Ёмкость тары алкогольной продукции в литрах\n"; } 
+					}else { alcholByVolume = parseFloat(buffStr); }
+				}
+				
+        let json = {
+          "uuid": uuid,
+          "code": code,
+          "barCodes": barCode,
+          "alcoCodes": alcoCodes,
+          "name": name,
+          "price": price,
+          "quantity": quantity,
+          "costPrice": costPrice,
+          "measureName": measureName,
+          "tax": tax,
+          "allowToSell": allowToSell,
+          "description": prd["Описание товара"] !== null ? prd["Описание товара"] : "",
+          "articleNumber": prd["Артикул"] !== null? prd["Артикул"] : "",
+          "parentUuid": "",
+          "group": false,
+          "type": typePrd,
+          "alcoholByVolume": alcholByVolume,
+          "alcoholProductKindCode": alchoholProductKindCode,
+          "tareVolume": tareVolume
+        }
+					
+				if (errors != "") {
+					alert(errors);
+				}
+				 console.log(json);
+				arrJs.push(json);
+      }
+		}
+  }
+    // console.log(count);
+		return arrJs;
+  }
+
+	static fixdata(data) {
+    console.log("hi")
+    var o = "",
+      l = 0,
+      w = 10240;
+    for (; l < data.byteLength / w; ++l) o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)));
+    o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)));
+    return o;
+  }
+
+  static upload(file) {
+    console.log("Upload Started");
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/excel/upload", true);
+    console.log("Sending file Started");
+    xhr.send(file);
+    return xhr.status;
+  }
+
   ngOnInit() {
 
   }
